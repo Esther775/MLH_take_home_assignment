c428977308fad848bfb379caf863bba60014714d
Esther Morris <EstherMorris775@gmail.com>
2022-11-09 09:48:03 -0500

    added message to tempalte

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,10 @@
 class ProjectsController < ApplicationController
   require "http"
 
-  def index
+  def show
     if params[:id] == 1
-      @array = HTTP.get("https://api.github.com/repos/nodejs/nodejs.org/commits?sha=main").parse(:json)
+      @array = HTTP.get("https://api.github.com/repos/nodejs/nodejs.org/commits?sha=main&per_page=100").parse(:json)
+      # @stats = HTTP.get("https://api.github.com/repos/nodejs/nodejs.org/stats/contributors").parse(:json)
       # render json: nodejs_array[0]["commit"]["author"]["name"].as_json
       # render json: @nodejs_array[0]
       render template: "projects/index"
@@ -17,26 +18,21 @@ class ProjectsController < ApplicationController
       render template: "invalid param id"
     end
   end
-
-  def show
-    nodejs_array = HTTP.get("https://api.github.com/repos/hursittarcan/nodejs.org/commits").parse(:json)
-  end
-
-  # {
-  #   "id": 1,
-  #   "contributors": [
-  #     { id: 1, github_username: "foobar" },
-  #     { id: 2, github_username: "foobar_1" },
-  #     { id: 3, github_username: "foobar_2" },
-  #     ...
-  #   ],
-  #   "contributions": {
-  #     "additions": "100000",
-  #     "deletions": "20000"
-  #   }
-  # }
-
 end
+
+# {
+#   "id": 1,
+#   "contributors": [
+#     { id: 1, github_username: "foobar" },
+#     { id: 2, github_username: "foobar_1" },
+#     { id: 3, github_username: "foobar_2" },
+#     ...
+#   ],
+#   "contributions": {
+#     "additions": "100000",
+#     "deletions": "20000"
+#   }
+# }
 
 #got the data
 # assign data to a variable
