4ca4c0039ba1bc9c30dbcb2a2af0edfe730f4487
Esther Morris <EstherMorris775@gmail.com>
2022-11-09 08:13:37 -0500

    call api from user params

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,13 +2,20 @@ class ProjectsController < ApplicationController
   require "http"
 
   def index
-    @nodejs_array = HTTP.get("https://api.github.com/repos/nodejs/nodejs.org/commits?sha=main").parse(:json)
-
-    # render json: nodejs_array[0]["commit"]["author"]["name"].as_json
-    # render json: @nodejs_array[0]
-    # render json: "hello"
-
-    render template: "projects/index"
+    if params[:id] == 1
+      @array = HTTP.get("https://api.github.com/repos/nodejs/nodejs.org/commits?sha=main").parse(:json)
+      # render json: nodejs_array[0]["commit"]["author"]["name"].as_json
+      # render json: @nodejs_array[0]
+      render template: "projects/index"
+    elsif params[:id] == 2
+      @array = HTTP.get("https://api.github.com/repos/eileencodes/rails/commits").parse(:json)
+      render template: "projects/index"
+    elsif params[:id] == 3
+      @array = HTTP.get("https://api.github.com/repos/XrXr/ruby/commits").parse(:json)
+      render template: "projects/index"
+    else
+      render template: "invalid param id"
+    end
   end
 
   def show
